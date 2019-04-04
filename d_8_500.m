d546d00023bb9ca0cb56bb5c7dc16010dea49154
kutaybersis <kutaybuyukyildiz@hacettepe.edu.tr>
2019-04-04 16:02:49 +0300

    Add files via upload

---
+++ d_8_500.m
@@ -0,0 +1,49 @@
+addpath( genpath( '/Users/bersiskutay/Documents/MATLAB/MNPBEM17' ) );
+
+epsb  = epsconst( 1 );
+epsAg = epstable( 'silver.dat' );
+epsAu = epstable( 'gold.dat' );
+
+epstab = { epsb, epsAu, epsAg };
+
+
+% ====================================
+
+
+partic = trisphere(400 , 100);
+
+partic1 = scale(partic,[1 0.1 0.1]);
+partic_1 = shift(partic1,[-54 , 0, 0]);
+
+
+partic2 = scale(partic,[1 0.1 0.1]);
+partic_2 = shift(partic2,[54, 0, 0 ]);
+
+p3 = comparticle( epstab, {partic_1, partic_2} , [  2,1 ; 2,1 ], 1,2 );
+
+
+
+
+
+
+
+exc = planewaveret( [ 1, 0, 0 ], [ 0, 0, 1 ] );
+
+bem = bemret( p3 );
+%surface charge
+enei = 500 ;
+sig = bem \ exc( p3, enei );
+% exxamine sig: sig1 sig2 h1 h2
+
+% sca = exc.sca( sig );                                
+% ext = exc.ext( sig );
+
+figure(41)
+%  plot surface charge at boundary outside
+plot( p3, real( sig.sig2 ) );% real part is physical
+%question: why complex functions?
+axis on
+xlabel('x'); ylabel('y'); zlabel('z'); 
+title('Distance = 8nm & Wavelength = 500nm ')
+%axis([-100 100 -100 100 -50 50 ])
+colorbar
